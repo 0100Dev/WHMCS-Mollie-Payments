e2bd7a50eb18f1e0034d2f75319dc127bd23766c
DeveloperBoy <stijnbannink40@gmail.com>
2018-11-01 20:39:31 +0100

    Create Dutch.php

---
+++ src/mollie/lang/Dutch.php
@@ -0,0 +1,24 @@
+<?php
+
+$_GATEWAYLANG = array();
+
+$_GATEWAYLANG['selectBank'] = 'Selecteer bank';
+
+$_GATEWAYLANG['errorTransactionNotFound'] = 'Transactie is niet gevonden. Probeer later opnieuw';
+
+$_GATEWAYLANG['checkPayment'] = 'We controleren je betaling, moment geduld...';
+
+$_GATEWAYLANG['payWithBanktransfer'] = 'Betaal met Bank overschrijving';
+$_GATEWAYLANG['payWithBelfius'] = 'Betaal met Belfius';
+$_GATEWAYLANG['payWithBitcoin'] = 'Betaal met Bitcoin';
+$_GATEWAYLANG['payWithCreditcard'] = 'Betaal met Creditcard';
+$_GATEWAYLANG['payWithIdeal'] = 'Betaal met iDeal';
+$_GATEWAYLANG['payWithBancontact'] = 'Betaal met Bancontact';
+$_GATEWAYLANG['payWithPaypal'] = 'Betaal met PayPal';
+$_GATEWAYLANG['payWithPaysafecard'] = 'Betaal met Paysafecard';
+$_GATEWAYLANG['payWithSofort'] = 'Betaal met Sofort Banking';
+$_GATEWAYLANG['payWithEps'] = 'Betaal met EPS';
+$_GATEWAYLANG['payWithGiftcard'] = 'Betaal met een cadeaukaart';
+$_GATEWAYLANG['payWithGiropay'] = 'Betaal met Giropay';
+$_GATEWAYLANG['payWithInghomepay'] = 'Betaal met ING Home\'Pay';
+$_GATEWAYLANG['payWithKbc'] = 'Betaal met KBC/CBC-betaalknop';
