df0149a4d8216977c9f527d8b5e5e594bbfba901
DeveloperBoy <stijnbannink40@gmail.com>
2018-11-01 20:40:11 +0100

    Create English.php

---
+++ src/mollie/lang/English.php
@@ -0,0 +1,25 @@
+<?php
+
+$_GATEWAYLANG = array();
+
+$_GATEWAYLANG['selectBank'] = 'Select bank';
+$_GATEWAYLANG['selectLater'] = 'Select later';
+
+$_GATEWAYLANG['errorTransactionNotFound'] = 'Transaction is not found. Please try again.';
+
+$_GATEWAYLANG['checkPayment'] = 'Checking your payment, please wait...';
+
+$_GATEWAYLANG['payWithBanktransfer'] = 'Pay with Bank Transfer';
+$_GATEWAYLANG['payWithBelfius'] = 'Pay with Belfius';
+$_GATEWAYLANG['payWithBitcoin'] = 'Pay with Bitcoin';
+$_GATEWAYLANG['payWithCreditcard'] = 'Pay with Credit Card';
+$_GATEWAYLANG['payWithIdeal'] = 'Pay with iDeal';
+$_GATEWAYLANG['payWithBancontact'] = 'Pay with Bancontact';
+$_GATEWAYLANG['payWithPaypal'] = 'Pay with PayPal';
+$_GATEWAYLANG['payWithPaysafecard'] = 'Pay with Paysafecard';
+$_GATEWAYLANG['payWithSofort'] = 'Pay with Sofort Banking';
+$_GATEWAYLANG['payWithEps'] = 'Pay with EPS';
+$_GATEWAYLANG['payWithGiftcard'] = 'Pay with a gift card';
+$_GATEWAYLANG['payWithGiropay'] = 'Pay with Giropay';
+$_GATEWAYLANG['payWithInghomepay'] = 'Pay with ING Home\'Pay';
+$_GATEWAYLANG['payWithKbc'] = 'Pay with KBC/CBC Payment Button';
